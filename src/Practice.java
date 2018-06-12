7d75f91f1818348a06c17d33a206e0152a504397
Sagarika <sagarikanayakjobs@gmail.com>
2018-06-12 22:03:04 +0530

    print hello world

--- src/Practice.java
+++ src/Practice.java
@@ -7,23 +7,8 @@ public class Practice {
 
 	public static void main(String[] args) {
 		
-		String s="thisisthhh";
-		char [] a = s.toCharArray();
-		
-		for (int j=0;j <a.length; j++)
-		{
-				    String x= Character.toString(a[j]);
-				    int c=0;
-				for(int i=0;i<a.length;i++)
-				{
-					String v=Character.toString(a[i]);
-					if(x.equals(v))
-					{
-						c=c+1;
-					}
-				}
-				System.out.println(x + ": Appears " + c + " :times");
-		}
+		char a ='"';
+		System.out.println(a+"hello world"+a);
 	
 	
 	}
